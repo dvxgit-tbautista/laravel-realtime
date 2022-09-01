1393029142966999ded1dd566ed1d66a2c20bb75
dvxgit-tbautista <110645703+dvxgit-tbautista@users.noreply.github.com>
2022-09-01 14:35:50 +0800

    Tried select and option

--- resources/views/sender.blade.php
+++ resources/views/sender.blade.php
@@ -1,7 +1,11 @@
 <form action="/sender" method="post">
     @csrf
 
-    <input type="text" name="text">
+    {{-- <input type="text" name="text"> --}}
+    <select name="text">
+        <option value="ON">ON</option>
+        <option value="OFF">OFF</option>
+    </select>
     <input type="submit">
 
 </form>
